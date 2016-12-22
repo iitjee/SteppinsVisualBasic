0e15a7a9c6b8dbeded550b2230c94c02a6992ead
iitjee <iit.jee.help.me@gmail.com>
2016-12-22 18:08:18 +0530

    Create Strings.vb

---
+++ Strings.vb
@@ -0,0 +1,32 @@
+'''
+(IMPORTANT) IF "73" < "9" Then 'True (alpha comparison)
+(IMPORTANT) If "abc"<"abcd" Then  'True (Substring rule)
+See Comparing Patterns
+'''
+
+
+
+
+Dim value As String = New String {"H", "E", "L", "L", "O");
+Dim strNumeric As String = Format(45000, "0,000") '45,000 is the new format stored in string
+Dim strCurrency As String = FormatCurrency(numeric)
+
+Dim hello As String = "Hello From Visual basic"
+Dim howdy As String = New String( 'now you get a drop down menu; click arrow keys 1 of 3
+Dim howdy As String = New String("Byee Visual Basic")
+
+Dim words As Array = hello.Split() //splits the string into an array of words delimiter is space.
+Dim letters As Char() = howdy.ToCharArray() //splits word into characters
+
+
+'Comparing Strings
+If "Xyz" = "Xyz" Then
+If "Xyz" = "XYZ" Then
+(IMPORTANT) IF "73" < "9" Then 'True (alpha comparison)
+(IMPORTANT) If "abc"<"abcd" Then  'True (Substring rule)
+
+
+'Comparing Patterns
+? - Any Single Character
+* Zero or more charaters
+# Any numeric digit(0-9)
