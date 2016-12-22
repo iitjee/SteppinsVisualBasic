cb3a0f3330688fc35e023aa48c772b24f5f9d1a0
iitjee <iit.jee.help.me@gmail.com>
2016-12-22 18:08:04 +0530

    Update ConditionaslAndLoops.vb

--- ConditionaslAndLoops.vb
+++ ConditionaslAndLoops.vb
@@ -1,19 +1,47 @@
+/*
+'break' in other langs == use 'exit' keyword
+
+*/
 
 
 
-
-If str1 = str2 AndElse x<>y Then
+If str1 = str2 AndElse x<>y Then 'Remember AndElse will shortckt whereas just And won't.   Also you can use x Not y instead of x<>y
   ...
-Else If x>y
+ElseIf x>y
   ...
 Else
   ...
 End If
 
 
+Select Case x
+  Case "FirstCase"
+    ...
+  Case "SecondCase"
+    ...
+  Case Else
+    ...
+    Return
+End Select
+  
 
+
+For myCountIndex As Integer = 1 To 3
+  ...
+Next
 
 For Each name As String In listOfNames
   ....
 Next
+
+While myCount<=3
+  ...
+End While
+
+
+
+Do 
+  ...
+Loop Until myCount<=3
+  ...
 
